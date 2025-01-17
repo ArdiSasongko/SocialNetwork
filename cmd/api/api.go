56074051ad77cfa2f850cedaf47d21e0fb4010c3
Ardi Sasongko <ardisasongko002@gmail.com>
2025-01-17 09:22:45 +0700

    create cors config

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -7,8 +7,10 @@ import (
 
 	"github.com/ArdiSasongko/SocialNetwork/cmd/api/v1/handlers"
 	"github.com/ArdiSasongko/SocialNetwork/cmd/api/v1/middlewares"
+	"github.com/ArdiSasongko/SocialNetwork/internal/env"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 )
 
 type application struct {
@@ -49,6 +51,16 @@ func (app *application) mount() http.Handler {
 	r.Use(middleware.RealIP)
 	r.Use(middleware.Logger)
 	r.Use(middleware.Recoverer)
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{env.GetString("ALLOWED_ORIGIN", "http://*")},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
 	r.Use(middleware.Timeout(60 * time.Second))
 
 	r.Route("/v1", func(r chi.Router) {
